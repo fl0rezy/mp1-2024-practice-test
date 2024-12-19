dade9a9c06d61f6106156295452b03dec6144e07
fl0rezy <voznesenskyd@gmail.com>
2024-12-19 10:42:07 +0300

    Update task1.c

--- 3824Б1ФИ1/05 Вознесенский Дмитрий Евгеньевич/task1.c
+++ 3824Б1ФИ1/05 Вознесенский Дмитрий Евгеньевич/task1.c
@@ -1,6 +1,15 @@
 // Задача 1. Дан непустой массив A, содержащий n целых чисел.
 // Найти сколько чисел в массиве меньше своего левого соседа.
-size_t task1(int A[], size_t n)
+size_t task2(double A[], size_t n)
 {
-  return -1;
+    if (n < 3) {
+        return 0;
+    }
+    size_t count = 0;
+    for (size_t i = 1; i < n - 1; ++i) {
+        if (A[i] >= A[i - 1] && A[i] >= A[i + 1]) {
+            count+=1;
+        }
+    }
+    return count;
 }
