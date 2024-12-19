2bc241e24532197e7ccd2f7c099f354654f15187
fl0rezy <voznesenskyd@gmail.com>
2024-12-19 10:42:50 +0300

    Update task2.c

--- 3824Б1ФИ1/05 Вознесенский Дмитрий Евгеньевич/task2.c
+++ 3824Б1ФИ1/05 Вознесенский Дмитрий Евгеньевич/task2.c
@@ -3,5 +3,14 @@
 // Локальный максимум – элемент, который >= левого и правого соседей.
 size_t task2(double A[], size_t n)
 {
-  return -1;
+    if (n < 3) {
+        return 0;
+    }
+    size_t count = 0;
+    for (size_t i = 1; i < n - 1; ++i) {
+        if (A[i] >= A[i - 1] && A[i] >= A[i + 1]) {
+            count+=1;
+        }
+    }
+    return count;
 }
